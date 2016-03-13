bcedf17601dd0a6d95ba19d6efe7282f2b82b4d8
bkienzle <bkienzle@gmail.com>
2016-03-13 04:33:13 -0400

    AI Cleanup

--- TicTacToe/AI.cs
+++ TicTacToe/AI.cs
@@ -4,7 +4,63 @@ namespace TicTacToe
 {
     static class AI
     {
+        #region Private Variables
         static int[,] GameState;
+        #endregion
+
+        #region Public Methods
+        public static int FindBestMove(int[,] TempGameState, int EnemyTeamMarker)
+        {
+            GameState = TempGameState;
+            int HumanTeamMarker = Game.GetEnemyTeam(EnemyTeamMarker);
+            int TakenCells = 0;
+            int CurrentBestMove = 0;
+            /* 123
+             * 456
+             * 789
+             */
+
+            foreach (int i in GameState)
+            {
+                if (i != (int)Game.Mark.Empty) TakenCells++;
+            }
+            if (TakenCells == 0) return 1; // 1 is always the best first move.
+            if (TakenCells == 1)
+            {
+                if (DoesStateExist("50")) return 5; // 5 is always the best second move if it's available
+                return 1; // we have to take the corner otherwise
+            }
+
+            // From here on out I'm using the strategy used in Newell and Simon's 1972 Tic Tac Toe program.
+
+            CurrentBestMove = ObviousMove(EnemyTeamMarker.ToString()); // Play obvious winning moves
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            CurrentBestMove = ObviousMove(HumanTeamMarker.ToString()); // Block obvious winning opponent moves
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            CurrentBestMove = Forks(EnemyTeamMarker.ToString()); // Looks for ways to create forks
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            CurrentBestMove = Forks(HumanTeamMarker.ToString()); // Looks for fork opportunities to block
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            if (DoesStateExist("50")) return 5; // play in the center if it's available
+
+            CurrentBestMove = OppositeCorner(HumanTeamMarker.ToString()); // play in an opposite corner
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            CurrentBestMove = EmptyCorner(); // play in an empty corner
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            CurrentBestMove = EmptySide(); // play in the middle of an empty side
+            if (CurrentBestMove != 0) return CurrentBestMove;
+
+            return 1; // this shouldn't ever be reached.
+        }
+        #endregion
+
+        #region Private Methods
         static string TranslatePosition(string Position)
         {
             switch (Position)
@@ -37,8 +93,8 @@ namespace TicTacToe
             //instead of something like this:
             //if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[0, 1] == Team && GameState[1, 0] == Team) return 1; // 1 3 5 empty, 2 4 our markers
             //
-            //we can just do DoesStateExist("10,30,50,2{0},4{0}", TeamString);
-            
+            //we can just do if (DoesStateExist("10,2t,3t".Replace("t", TeamString))) return 1;
+
             foreach (string ThisEvaluate in Evaluate.Split(','))
             {
                 string xy = TranslatePosition(ThisEvaluate.Substring(0, 1));
@@ -49,127 +105,108 @@ namespace TicTacToe
             }
             return true;
         }
-        static int ObviousMove(int Team)
+        static int ObviousMove(string Team)
         {
+            // up and down
             for (int i = 0; i <= 2; i++)
             {
-                if (GameState[i, 0] == Team && GameState[i, 1] == Team && GameState[i, 2] == (int)Game.Mark.Empty) return (i + 1) * 3; // 3 6 or 9
-                if (GameState[i, 0] == Team && GameState[i, 1] == (int)Game.Mark.Empty && GameState[i, 2] == Team) return ((i + 1) * 3) - 1; // 2 5 or 8
-                if (GameState[i, 0] == (int)Game.Mark.Empty && GameState[i, 1] == Team && GameState[i, 2] == Team) return ((i + 1) * 3) - 2; // 1 4 or 7
-                if (GameState[0, i] == Team && GameState[1, i] == Team && GameState[2, i] == (int)Game.Mark.Empty) return (i + 7); // 7 8 or 9
-                if (GameState[0, i] == Team && GameState[1, i] == (int)Game.Mark.Empty && GameState[2, i] == Team) return (i + 4); // 4 5 or 6
-                if (GameState[0, i] == (int)Game.Mark.Empty && GameState[1, i] == Team && GameState[2, i] == Team) return (i + 1); // 1 2 or 3
+                if (DoesStateExist("10,2t,3t".Replace("t", Team))) return 1;
+                if (DoesStateExist("20,1t,3t".Replace("t", Team))) return 2;
+                if (DoesStateExist("30,1t,2t".Replace("t", Team))) return 3;
+                if (DoesStateExist("40,5t,6t".Replace("t", Team))) return 4;
+                if (DoesStateExist("50,4t,6t".Replace("t", Team))) return 5;
+                if (DoesStateExist("60,4t,5t".Replace("t", Team))) return 6;
+                if (DoesStateExist("70,8t,9t".Replace("t", Team))) return 7;
+                if (DoesStateExist("80,7t,9t".Replace("t", Team))) return 8;
+                if (DoesStateExist("90,7t,8t".Replace("t", Team))) return 9;
+
+                if (DoesStateExist("10,4t,7t".Replace("t", Team))) return 1;
+                if (DoesStateExist("20,5t,8t".Replace("t", Team))) return 2;
+                if (DoesStateExist("30,6t,9t".Replace("t", Team))) return 3;
+                if (DoesStateExist("40,1t,7t".Replace("t", Team))) return 4;
+                if (DoesStateExist("50,2t,8t".Replace("t", Team))) return 5;
+                if (DoesStateExist("60,3t,9t".Replace("t", Team))) return 6;
+                if (DoesStateExist("70,1t,4t".Replace("t", Team))) return 7;
+                if (DoesStateExist("80,2t,5t".Replace("t", Team))) return 8;
+                if (DoesStateExist("90,3t,6t".Replace("t", Team))) return 9;
             }
-
-            if (GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 0] == Team && GameState[1, 1] == Team) return 9; // 9 empty, 1 5 our markers
-            if (GameState[1, 1] == (int)Game.Mark.Empty && GameState[0, 0] == Team && GameState[2, 2] == Team) return 5; // 5 empty, 1 9 our markers
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[1, 1] == Team && GameState[2, 2] == Team) return 1; // 1 empty, 5 9 our markers
-
-            if (GameState[0, 2] == (int)Game.Mark.Empty && GameState[1, 1] == Team && GameState[2, 0] == Team) return 3; // 3 empty, 5 7 our markers
-            if (GameState[1, 1] == (int)Game.Mark.Empty && GameState[0, 2] == Team && GameState[2, 0] == Team) return 5; // 5 empty, 3 7 our markers
-            if (GameState[2, 0] == (int)Game.Mark.Empty && GameState[0, 2] == Team && GameState[1, 1] == Team) return 7; // 7 empty, 3 5 our markers
+            //diagonals
+            if (DoesStateExist("90,1t,5t".Replace("t", Team))) return 9;
+            if (DoesStateExist("50,1t,9t".Replace("t", Team))) return 5;
+            if (DoesStateExist("10,5t,9t".Replace("t", Team))) return 1;
+            if (DoesStateExist("30,5t,7t".Replace("t", Team))) return 3;
+            if (DoesStateExist("50,3t,7t".Replace("t", Team))) return 5;
+            if (DoesStateExist("70,3t,5t".Replace("t", Team))) return 7;
 
             return 0;
         }
-        static int Forks(int Team)
+        static int Forks(string Team)
         {
-            //corner spots first, since we only need to do those once.  see the tic tac toe diagram if the numbers dont make sense.
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[0, 1] == Team && GameState[1, 0] == Team) return 1; // 1 3 5 empty, 2 4 our markers
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 1] == Team && GameState[1, 2] == Team) return 3; // 1 3 9 empty, 2 6 our markers
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[1, 0] == Team && GameState[2, 1] == Team) return 7; // 1 7 9 empty, 4 8 our markers
-            if (GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[1, 2] == Team && GameState[2, 1] == Team) return 9; // 3 7 9 empty, 6 8 our markers
+            //corner spots first, since we only need to do those once.
+            if (DoesStateExist("10,30,50,2t,4t".Replace("t", Team))) return 1;
+            if (DoesStateExist("10,30,90,2t,6t".Replace("t", Team))) return 3;
+            if (DoesStateExist("10,70,90,4t,8t".Replace("t", Team))) return 7;
+            if (DoesStateExist("30,70,90,6t,8t".Replace("t", Team))) return 9;
 
             //now the sides, we need to have the 5 position for any of these to work. 5 is implied as being part of the "our markers" group in the comments.
-            if (GameState[1, 1] == Team)
+            if (DoesStateExist("5t".Replace("t", Team)))
             {
-                if (GameState[0, 1] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 1] == (int)Game.Mark.Empty && GameState[0, 0] == Team) return 2; // 2 3 8 empty, 1 our markers
-                if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 1] == (int)Game.Mark.Empty && GameState[2, 1] == (int)Game.Mark.Empty && GameState[0, 2] == Team) return 2; // 1 2 8 empty, 3 our markers
-
-                if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[1, 0] == (int)Game.Mark.Empty && GameState[1, 2] == (int)Game.Mark.Empty && GameState[2, 0] == Team) return 4; // 1 4 6 empty, 7 our markers
-                if (GameState[1, 0] == (int)Game.Mark.Empty && GameState[1, 2] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[0, 0] == Team) return 4; // 4 6 7 empty, 1 our markers
-
-                if (GameState[1, 0] == (int)Game.Mark.Empty && GameState[1, 2] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 2] == Team) return 6; // 4 6 9 empty, 3 our markers
-                if (GameState[0, 2] == (int)Game.Mark.Empty && GameState[1, 0] == (int)Game.Mark.Empty && GameState[1, 2] == (int)Game.Mark.Empty && GameState[2, 2] == Team) return 6; // 3 4 6 empty, 9 our markers
-
-                if (GameState[0, 1] == (int)Game.Mark.Empty && GameState[2, 1] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[2, 0] == Team) return 8; // 2 8 9 empty, 7 our markers
-                if (GameState[0, 1] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[2, 1] == (int)Game.Mark.Empty && GameState[2, 2] == Team) return 8; // 2 7 8 empty, 9 our markers
+                if (DoesStateExist("20,30,80,1t".Replace("t", Team))) return 2;
+                if (DoesStateExist("10,20,80,3t".Replace("t", Team))) return 2;
+                if (DoesStateExist("10,40,60,7t".Replace("t", Team))) return 4;
+                if (DoesStateExist("40,60,70,1t".Replace("t", Team))) return 4;
+                if (DoesStateExist("40,60,90,3t".Replace("t", Team))) return 6;
+                if (DoesStateExist("30,40,60,9t".Replace("t", Team))) return 6;
+                if (DoesStateExist("20,80,90,7t".Replace("t", Team))) return 8;
+                if (DoesStateExist("20,70,80,9t".Replace("t", Team))) return 8;
             }
 
             //these last ones need 5 to be an option for us, since that will be our return value if we get a hit.
-            if (GameState[1,1] == (int)Game.Mark.Empty)
+            if (DoesStateExist("50"))
             {
-                if (GameState[2, 1] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 0] == Team && GameState[0, 1] == Team) return 5; // 8 9 empty, 1 2 our markers
-                if (GameState[2, 0] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 1] == Team && GameState[0, 2] == Team) return 5; // 7 8 empty, 2 3 our markers
-                if (GameState[1, 0] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[0, 2] == Team && GameState[1, 2] == Team) return 5; // 4 7 empty, 3 6 our markers
-                if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[1, 0] == (int)Game.Mark.Empty && GameState[1, 2] == Team && GameState[2, 2] == Team) return 5; // 1 4 empty, 6 9 our markers
-                if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 1] == (int)Game.Mark.Empty && GameState[2, 1] == Team && GameState[2, 2] == Team) return 5; // 1 2 empty, 8 9 our markers
-                if (GameState[0, 1] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 0] == Team && GameState[2, 1] == Team) return 5; // 2 3 empty, 7 8 our markers
-                if (GameState[0, 2] == (int)Game.Mark.Empty && GameState[1, 2] == (int)Game.Mark.Empty && GameState[1, 0] == Team && GameState[2, 0] == Team) return 5; // 3 6 empty, 4 7 our markers
-                if (GameState[1, 2] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 0] == Team && GameState[1, 0] == Team) return 5; // 6 9 empty, 1 4 our markers
-
-                if (GameState[2, 0] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 0] == Team && GameState[0, 2] == Team) return 5; // 7 9 empty, 1 3 our markers
-                if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 0] == Team && GameState[2, 2] == Team) return 5; // 1 3 empty, 7 9 our markers
-                if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[2, 0] == (int)Game.Mark.Empty && GameState[0, 2] == Team && GameState[2, 2] == Team) return 5; // 1 7 empty, 3 9 our markers
-                if (GameState[0, 2] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty && GameState[0, 0] == Team && GameState[2, 0] == Team) return 5; // 3 9 empty, 1 7 our markers
+                if (DoesStateExist("80,90,1t,2t".Replace("t", Team))) return 5;
+                if (DoesStateExist("70,80,2t,3t".Replace("t", Team))) return 5;
+                if (DoesStateExist("40,70,3t,6t".Replace("t", Team))) return 5;
+                if (DoesStateExist("10,40,6t,9t".Replace("t", Team))) return 5;
+                if (DoesStateExist("10,20,8t,9t".Replace("t", Team))) return 5;
+                if (DoesStateExist("20,30,7t,8t".Replace("t", Team))) return 5;
+                if (DoesStateExist("30,60,4t,7t".Replace("t", Team))) return 5;
+                if (DoesStateExist("60,90,1t,4t".Replace("t", Team))) return 5;
+                if (DoesStateExist("70,90,1t,3t".Replace("t", Team))) return 5;
+                if (DoesStateExist("10,30,7t,9t".Replace("t", Team))) return 5;
+                if (DoesStateExist("10,70,3t,9t".Replace("t", Team))) return 5;
+                if (DoesStateExist("30,90,10,7t".Replace("t", Team))) return 5;
             }
 
             return 0;
         }
-        public static int FindBestMove(int[,] TempGameState, int EnemyTeamMarker)
+        static int OppositeCorner(string Team)
         {
-            GameState = TempGameState;
-            int HumanTeamMarker = Game.GetEnemyTeam(EnemyTeamMarker);
-            int TakenCells = 0;
-            int CurrentBestMove = 0;
-            /* 123
-             * 456
-             * 789
-             */
-            // determine which game move
+            if (DoesStateExist("1t,90".Replace("t", Team))) return 9;
+            if (DoesStateExist("3t,70".Replace("t", Team))) return 7;
+            if (DoesStateExist("9t,10".Replace("t", Team))) return 1;
+            if (DoesStateExist("7t,30".Replace("t", Team))) return 3;
 
-            foreach (int i in GameState)
-            {
-                if (i != (int)Game.Mark.Empty) TakenCells++;
-            }
-            if (TakenCells == 0) return 1; // 1 is always the best first move.
-            if (TakenCells == 1)
-            {
-                if (GameState[1, 1] == (int)Game.Mark.Empty) return 5; // 5 is always the best second move if it's available
-                return 1;
-            }
-
-            // From here on out I'm using the strategy used in Newell and Simon's 1972 Tic Tac Toe program.
-
-            CurrentBestMove = ObviousMove(EnemyTeamMarker); // Play obvious winning moves
-            if (CurrentBestMove != 0) return CurrentBestMove;
-
-            CurrentBestMove = ObviousMove(HumanTeamMarker); // Block obvious winning opponent moves
-            if (CurrentBestMove != 0) return CurrentBestMove;
-
-            CurrentBestMove = Forks(EnemyTeamMarker); // Looks for ways to create forks
-            if (CurrentBestMove != 0) return CurrentBestMove;
-
-            CurrentBestMove = Forks(HumanTeamMarker); // Looks for fork opportunities to block
-            if (CurrentBestMove != 0) return CurrentBestMove;
-
-            if (GameState[1, 1] == (int)Game.Mark.Empty) return 5; // play in the center if it's available
-
-            if (GameState[0, 0] == HumanTeamMarker && GameState[2, 2] == (int)Game.Mark.Empty) return 9; // play in an opposite corner
-            if (GameState[0, 2] == HumanTeamMarker && GameState[2, 0] == (int)Game.Mark.Empty) return 7;
-            if (GameState[2, 2] == HumanTeamMarker && GameState[0, 0] == (int)Game.Mark.Empty) return 1;
-            if (GameState[2, 0] == HumanTeamMarker && GameState[0, 2] == (int)Game.Mark.Empty) return 3;
-
-            if (GameState[2, 2] == (int)Game.Mark.Empty) return 9; // play in an empty corner
-            if (GameState[2, 0] == (int)Game.Mark.Empty) return 7;
-            if (GameState[0, 0] == (int)Game.Mark.Empty) return 1;
-            if (GameState[0, 2] == (int)Game.Mark.Empty) return 3;
-
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 1] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty) return 2; //play in the middle of an empty side
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 1] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty) return 4;
-            if (GameState[0, 0] == (int)Game.Mark.Empty && GameState[0, 1] == (int)Game.Mark.Empty && GameState[0, 2] == (int)Game.Mark.Empty) return 6;
-            if (GameState[2, 0] == (int)Game.Mark.Empty && GameState[2, 1] == (int)Game.Mark.Empty && GameState[2, 2] == (int)Game.Mark.Empty) return 8;
-
-            return 1; // this shouldn't ever be reached.
+            return 0;
         }
+        static int EmptyCorner()
+        {
+            if (DoesStateExist("90")) return 9;
+            if (DoesStateExist("70")) return 7;
+            if (DoesStateExist("10")) return 1;
+            if (DoesStateExist("30")) return 3;
+
+            return 0;
+        }
+        static int EmptySide()
+        {
+            if (DoesStateExist("10,20,30")) return 2;
+            if (DoesStateExist("10,40,70")) return 4;
+            if (DoesStateExist("30,60,90")) return 6;
+            if (DoesStateExist("70,80,90")) return 8;
+
+            return 0;
+        }
+        #endregion
     }
 }
